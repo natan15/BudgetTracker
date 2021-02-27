60f235bfb2b4d5a2135cc10226139064821a5161
Natan Stuart <nbs2115@gmail.com>
2021-02-26 17:48:31 -0800

    deploying

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 const app = express();
 
