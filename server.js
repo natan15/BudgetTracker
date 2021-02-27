1826ed9b56dcb4a14d6c5c7a5abed14b8e5030fb
Natan Stuart <nbs2115@gmail.com>
2021-02-26 19:34:10 -0800

    fixed connection

--- server.js
+++ server.js
@@ -24,12 +24,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
